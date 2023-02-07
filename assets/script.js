ef2b895aee9981b16fc6fc7f72e6983f5e098337
LHardyWilcox <motacycaryda@mac.com>
2023-02-06 21:31:29 -0800

    code updates

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
+var submitBtn = document.querySelector('.submit');
+var cityLat = 0;
+var cityLon = 0;
 
 
+function getCoord() {
+    var cityState = document.getElementById('search').value;
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityState + '&appid=62b2ebee1dd3d81b4b4a10b045e5f835'
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+/* function getCurrentWeather() {
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+}
 
 
+function getFiveDay() {
 
+    var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+} */
 
-
-
+submitBtn.addEventListener('click', getCoord);
